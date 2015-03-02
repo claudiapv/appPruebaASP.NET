daed55204179b7ca4f5aa7daa92a2334dcb4f4d6
claudiapv <claupapuva@gmail.com>
2015-03-02 18:58:04 -0500

    hoy

---
+++ APP/Modelo/Personas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APP.Modelo
+{
+    public class Personas
+    {
+
+        private string id;
+
+        private string nombre;
+
+        private string direccion;
+
+        private string telefonos;
+
+        private string email;
+
+     
+        
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        public string Telefonos
+        {
+            get { return telefonos; }
+            set { telefonos = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
